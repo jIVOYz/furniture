b7050f7cb961db84626a5e6506d6717062c9e622
jIVOYz <80265811+jIVOYz@users.noreply.github.com>
2022-11-01 15:10:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+
+    navigation: {
+        nextEl: ".swipe_btn_next",
+        prevEl: ".swiper_btn_prev",
+    },
+});
+    
+const mainElement = document.documentElement;
+const screenWidth = mainElement.clientWidth;
+const screenHeight = mainElement.clientHeight;
+
+console.log(`Your screen width: ${screenWidth}`);
+console.log(`Your screen height: ${screenHeight}`);
+
+if(screenWidth <= 480) {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    slidesPerGroup: 0,
+    loop: false,
+    loopFillGroupWithBlank: false,
+
+});
+}
+
