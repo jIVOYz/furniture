72124319773fb9a52d2a0f3a2491229c071cbc5d
jIVOYz <ilaprohorov51@gmail.com>
2023-09-22 20:53:18 +0300

    Added comments

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
+// Swiper
 var swiper = new Swiper(".swiper", {
-    slidesPerView: 4,
-    spaceBetween: 40,
-    slidesPerGroup: 4,
-    loop: true,
-    loopFillGroupWithBlank: true,
+  slidesPerView: 4,
+  spaceBetween: 40,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
 
-    navigation: {
-        nextEl: ".swipe_btn_next",
-        prevEl: ".swiper_btn_prev",
-    },
-});
-    
-const mainElement = document.documentElement;
-const screenWidth = mainElement.clientWidth;
-const screenHeight = mainElement.clientHeight;
+  navigation: {
+    nextEl: ".swipe_btn_next",
+    prevEl: ".swiper_btn_prev",
+  },
+})
 
-console.log(`Your screen width: ${screenWidth}`);
-console.log(`Your screen height: ${screenHeight}`);
+const mainElement = document.documentElement
+const screenWidth = mainElement.clientWidth
+const screenHeight = mainElement.clientHeight
 
-if(screenWidth <= 480) {
+console.log(`Your screen width: ${screenWidth}`)
+console.log(`Your screen height: ${screenHeight}`)
+
+// Show less cards on small screens
+if (screenWidth <= 480) {
   var swiper = new Swiper(".swiper", {
     slidesPerView: 2,
     spaceBetween: 0,
     slidesPerGroup: 0,
     loop: false,
     loopFillGroupWithBlank: false,
-
-});
+  })
 }
-
